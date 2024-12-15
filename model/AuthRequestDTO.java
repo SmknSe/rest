4058f735dcb334a8fa628f22b3d8e75b423ac3f8
SmknSe <semeykin2003@mail.ru>
2024-12-15 16:18:11 +0300

    dto mapping

---
+++ model/AuthRequestDTO.java
@@ -0,0 +1,18 @@
+package model;
+
+import com.example.composer.configuration.GrantedAuthorityDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+public record AuthRequestDTO(
+        UUID id,
+        String name,
+        Map<String, Object>attributes,
+        @JsonDeserialize(contentUsing = GrantedAuthorityDeserializer.class)
+        Set<GrantedAuthority>authorities
+) {
+}
