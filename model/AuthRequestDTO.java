5b2575c38bc3d0bb9175e55abe49c3f2a2b25c39
SmknSe <semeykin2003@mail.ru>
2024-12-16 15:27:31 +0300

    encoder

--- model/AuthRequestDTO.java
+++ model/AuthRequestDTO.java
@@ -1,18 +1,9 @@
 package model;
 
-import util.GrantedAuthorityDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
 public record AuthRequestDTO(
         UUID id,
-        String name,
-        Map<String, Object> attributes,
-        @JsonDeserialize(contentUsing = GrantedAuthorityDeserializer.class)
-        Set<GrantedAuthority> authorities
+        String name
 ) {
 }
