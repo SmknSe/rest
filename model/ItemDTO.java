ec4ff61b4a4c95d912bc9a95f43279d8cbd82533
SmknSe <semeykin2003@mail.ru>
2024-12-19 02:06:33 +0300

    mapper fixes

--- model/ItemDTO.java
+++ model/ItemDTO.java
@@ -1,8 +1,10 @@
 package model;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record ItemDTO(
+        UUID id,
         String name,
         String description,
         BigDecimal price
